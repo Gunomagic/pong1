9b9d045fda1a4eafc17372ae58c0d71d17e59b50
Gunomagic <Gunomagic@users.noreply.github.com>
2020-03-30 13:33:25 +0530

    Update main.lua
    added fullscreen mode

--- pong/main.lua
+++ pong/main.lua
@@ -82,7 +82,7 @@ function love.load()
 
     -- initialize window with virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
+        fullscreen = true,
         resizable = true,
         vsync = true
     })
